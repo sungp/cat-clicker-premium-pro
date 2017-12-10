2c65add1d250bac0fb67826fbf8d2341d18c6f8c
Sung <sungp2k@gmail.com>
2017-12-10 08:53:40 -0800

    sidebar render works

--- js/script.js
+++ js/script.js
@@ -1,26 +1,101 @@
+$(function() {
+    class Cat {
+        constructor(name, counter, filename) {
+            this.name = name;
+            this.counter = counter;
+            this.filename = filename;
+        }
+        clicked(){
+            this.counter += 1;
+            return this.counter;
+        }
+    }
+    var model = {
+        init: function() {
+            var cats = new Map();
+            cats.set("Bolly", new Cat("Bolly", 0, "cat1.jpg"));
+            cats.set("Dolly", new Cat("Dolly", 0, "cat2.jpg"));
+            cats.set("Folly", new Cat("Folly", 0, "cat3.jpg"));
+            cats.set("Golly", new Cat("Golly", 0, "cat4.jpg"));
+            cats.set("Molly", new Cat("Molly", 0, "cat5.jpg"));
+            //console.log(cats);
+            var serializedCats = JSON.stringify(Array.from(cats.entries())); 
+            localStorage.cats = serializedCats;
+            //console.log(serializedCats);
+            this.currentlySelected = "Bolly";
+        },
+        getCats: function() {
+            if (!localStorage.cats) {
+                return Map.empty();
+            } else {
+                return new Map(JSON.parse(localStorage.cats)); 
+            }
+        },
+        setCats: function(cats) {
+            localStorage.cats = JSON.stringify(Array.from(cats.entries()));
+        },
+        clicked: function(catName) {
+            getCats().get(catName).clicked();
+        },
+        selectACat: function(catName) {
+            this.currentlySelected = catName;
+        },
+        
+        cats: function() {
+            var cats = new Map(JSON.parse(localStorage.cats));
+            //console.log(cats);
+            return Array.from(cats.keys());
+        }
+    };
 
-var $body = $('body');
+    var octopus = {
+        init: function() {
+            model.init();
+            sidebarView.init();
+        },
+        selectCat: function(catName) {
+            model.selectACat(catName);
+            alert(catName + " selected");
+        },
+        getCatNames: function() {
+            return model.cats();
+        }
+    };
+
+    var sidebarView = {
+        init: function() {
+            sidebarView.render();
+        },
+        render: function() {
+            octopus.getCatNames().forEach(function(catName) {
+                //console.log(catName);
+                $('#sidebar').append(
+                    $('<li>').append(
+                        $('<a>', {
+                            href: '#',
+                            id: catName,
+                            text: catName
+                        })
+                    )
+                );
+
+                $('#' + catName).click(function(e) {
+                    console.log(catName + " selected in the view");
+                    octopus.selectCat(catName);
+                });
+            });
+        }
+    };
+
+    octopus.init();
+});
+
+
+/*var $body = $('body');
 var content = $('#content');
-var sidebar = $('#sidebar');
+var sidebar = 
+*/
 
-class Cat {
-    constructor(name, counter, filename) {
-        this.name = name;
-        this.counter = counter;
-        this.filename = filename;
-    }
-    clicked(){
-        this.counter += 1;
-        return this.counter;
-    }
-}
-
-var cats = [];
-cats.push(new Cat("Bolly", 0, "cat1.jpg"));
-cats.push(new Cat("Dolly", 0, "cat2.jpg"));
-cats.push(new Cat("Folly", 0, "cat3.jpg"));
-cats.push(new Cat("Golly", 0, "cat4.jpg"));
-cats.push(new Cat("Molly", 0, "cat5.jpg"));
 //initialize
 
 
@@ -28,7 +103,7 @@ cats.push(new Cat("Molly", 0, "cat5.jpg"));
 /*var cats = [{ img: "cat1", counter: "click1" }, 
     {img: "cat2", counter: "click2" }];
 */
-$.each( cats, function(index, cat) {
+/*$.each( localStorage.cats, function(key, cat) {
     sidebar.append(
         $('<li>').append(
             $('<a>', {
@@ -84,3 +159,4 @@ $.each( cats, function(index, cat) {
     });
 
 });
+*/
