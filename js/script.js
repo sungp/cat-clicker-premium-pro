b4c0ba26a1a3e31a8c0d8e31fc678a33109d17b4
Sung <sungp2k@gmail.com>
2017-12-11 07:13:34 -0800

    both sideview contentiew works

--- js/script.js
+++ js/script.js
@@ -34,13 +34,29 @@ $(function() {
         setCats: function(cats) {
             localStorage.cats = JSON.stringify(Array.from(cats.entries()));
         },
-        clicked: function(catName) {
-            getCats().get(catName).clicked();
-        },
         selectACat: function(catName) {
             this.currentlySelected = catName;
         },
-        
+        getSelectedCat: function() {
+            return this.getCats().get(this.currentlySelected);
+        },
+        getSelectedCatName: function() {
+            return this.currentlySelected;
+        },
+        getSelectedCatCount: function() {
+            return this.getSelectedCat().counter; 
+        },
+        getSelectedCatImage: function() {
+            return this.getSelectedCat().filename;
+        },
+        selectedCatClicked: function() {
+            var cats = this.getCats();
+            var catObj = cats.get(this.currentlySelected);
+            var cat = new Cat(catObj.name, catObj.counter, catObj.filename);
+            cat.clicked();
+            cats.set(this.currentlySelected, cat);
+            this.setCats(cats); 
+        },
         cats: function() {
             var cats = new Map(JSON.parse(localStorage.cats));
             //console.log(cats);
@@ -52,33 +68,55 @@ $(function() {
         init: function() {
             model.init();
             sidebarView.init();
+            contentView.init();
         },
         selectCat: function(catName) {
             model.selectACat(catName);
-            alert(catName + " selected");
+            contentView.render();
+            console.log(catName + " selected");
         },
         getCatNames: function() {
             return model.cats();
+        },
+        getSelectedCatName: function() {
+            return model.getSelectedCatName(); 
+        },
+        getSelectedCatCount: function() {
+            return model.getSelectedCatCount(); 
+        },
+        getSelectedCatImage: function() {
+            return model.getSelectedCatImage(); 
+        },
+        selectedCatClicked: function() {
+            model.selectedCatClicked();
+            contentView.render();
         }
     };
 
     var sidebarView = {
         init: function() {
+            var catElems = new Map();
+            octopus.getCatNames().forEach(function(catName) {
+                catElems.set(catName, 
+                    $('<a>', {
+                        href: '#',
+                        id: catName,
+                        text: catName
+                    })
+                );
+            });
+            this.catElems = catElems;
             sidebarView.render();
         },
         render: function() {
+            //console.log(this.catElems);
+            var elems = this.catElems;
             octopus.getCatNames().forEach(function(catName) {
                 //console.log(catName);
+                var myElem = elems.get(catName);
                 $('#sidebar').append(
-                    $('<li>').append(
-                        $('<a>', {
-                            href: '#',
-                            id: catName,
-                            text: catName
-                        })
-                    )
+                    $('<li>').append(myElem)
                 );
-
                 $('#' + catName).click(function(e) {
                     console.log(catName + " selected in the view");
                     octopus.selectCat(catName);
@@ -86,6 +124,60 @@ $(function() {
             });
         }
     };
+
+    var contentView = {
+        init: function() {
+            contentView.render();
+        },
+        render: function() {
+            $('#content').empty();
+            var catName = octopus.getSelectedCatName();
+            var catCount = octopus.getSelectedCatCount();
+            var catImage = octopus.getSelectedCatImage();
+            var imgid = catName + "-img";
+            var countid = catName + "-count";
+            var counterString = 'Your clicks for ' + catName + ' so far: ';
+            $('#content').append(
+                $('<div>', {'class': 'row'}).append(
+                    $('<h1>', {
+                        'class': 'my-4',
+                        text: 'Here is ' + catName
+                    })
+                )
+            )
+            .append(
+                $('<div>', {'class': 'container'}).append(
+                    $('<div>', {
+                        'class': 'col-md-8',
+                    }).append(   
+                        $('<img>', {
+                            id:  imgid,
+                            'style': "max-height: 100%; max-width: 100%;",
+                            'src': catImage 
+                        })
+                    )
+                )
+            )
+            .append(
+                $('<div>', {'class': 'row'}).append(
+                    $('<h1>', {
+                        id: countid, 
+                        'class': 'my-3',
+                        text: counterString + catCount.toString()
+                    })
+                )
+            );
+            var imgElem = $('#' + imgid);
+            var countElem = $('#' + countid);
+            imgElem.click(function(e) {
+                console.log(catName + "image clicked");
+                octopus.selectCat(catName);
+                octopus.selectedCatClicked();
+                countElem.text(counterString + 
+                    octopus.getSelectedCatCount().toString());
+            });
+        }
+    }
 
     octopus.init();
 });
